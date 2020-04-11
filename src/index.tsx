75da9172133c1eed03dd8ba2a63ba0532a491ec4
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-04-11 18:47:40 +0300

    Adjust global styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'typeface-montserrat';
 import { App } from './App';
-import { GlobalStyle } from './global.styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
