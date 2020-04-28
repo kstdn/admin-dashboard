0b94a15179c4d8688265accd0c5632dc157fb7ff
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-04-28 16:32:13 +0300

    Add fillContainer prop to Box

--- src/shared/components/Box/BoxProps.ts
+++ src/shared/components/Box/BoxProps.ts
@@ -3,4 +3,5 @@ import { Color } from 'shared/color.type';
 export type BoxProps = {
   color?: Color;
   inverted?: boolean;
+  fillContainer?: boolean;
 };
--- src/shared/components/Box/index.tsx
+++ src/shared/components/Box/index.tsx
@@ -4,5 +4,11 @@ import { BoxProps } from './BoxProps';
 
 const Box = styled.div<BoxProps>`
   ${props => getBaseStyles(props)}
+  ${props =>
+    props.fillContainer &&
+    `
+    height: 100%;
+    width: 100%;
+  `}
 `;
 export default Box;
