a1148bd62fc70f6c9b95f896d93ef4a3ef2b7e9d
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-05-23 01:29:04 +0300

    Add Flex component

---
+++ src/shared/components/Flex/styled.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/macro';
+
+type Props = {
+  gap?: boolean;
+};
+
+export const Flex = styled.div<Props>`
+  display: flex;
+  ${props =>
+    props.gap &&
+    `
+    & > *:not(:last-of-type) {
+    margin-right: var(--base-padding);
+  }`}
+`;
