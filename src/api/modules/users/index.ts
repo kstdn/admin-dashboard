553ccc59e36389fcfa78e366a999b95fc88953d2
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-05-23 01:24:45 +0300

    Fix httpClient import

--- src/api/modules/users/index.ts
+++ src/api/modules/users/index.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
+import { httpClient } from 'util/http-client';
 import { ApiRoute } from './../../api-route';
 import { UserDto } from './dto/user.dto';
 
 export const getOwnUser = () => {
-  return axios.get<UserDto, UserDto>(ApiRoute.UsersMe);
+  return httpClient.get<UserDto, UserDto>(ApiRoute.UsersMe);
 };
 
 export const getUsers = () => {
-  return axios.get<UserDto[], UserDto[]>(ApiRoute.Users);
+  return httpClient.get<UserDto[], UserDto[]>(ApiRoute.Users);
 };
