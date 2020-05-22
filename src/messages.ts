9ed1b10611dc4f18179426b1e0884f8a262165f1
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-05-23 01:23:38 +0300

    Add common messages

--- src/messages.ts
+++ src/messages.ts
@@ -1,2 +1,5 @@
 export const NO_USERNAME_IN_ACCESS_TOKEN = 'No username found in access token';
 export const GENERIC_ERROR = 'There has been an error';
+export const CREATE_ERROR = 'Create unsuccessful';
+export const UPDATE_ERROR = 'Update unsuccessful';
+export const DELETE_ERROR = 'Delete unsuccessful';
