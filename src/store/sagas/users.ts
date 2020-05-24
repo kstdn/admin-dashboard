995f3ccff12f60bb4567fb71ed35e502e72662ad
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-05-25 00:46:26 +0300

    Add takeAndValidateCache higher order saga

--- src/store/sagas/users.ts
+++ src/store/sagas/users.ts
@@ -1,18 +1,21 @@
 import { getUsers } from 'api';
 import { GENERIC_ERROR } from 'messages';
-import { all, fork, put, takeLeading } from 'redux-saga/effects';
+import { all, fork, put, select } from 'redux-saga/effects';
 import * as fromUsers from 'store/slices/Users';
+import { takeAndValidateCache } from './common';
 const {
   actions: { load, loadSuccess, loadFailure },
 } = fromUsers.slice;
 
 function* watchLoad() {
-  yield takeLeading(load.type, loadWorker);
+  yield takeAndValidateCache(load.type, 10000, loadWorker);
 }
 
-function* loadWorker() {
+function* loadWorker(cacheIsInvalid: boolean) {
   try {
-    const users = yield getUsers();
+    const users = cacheIsInvalid
+      ? yield getUsers()
+      : yield select(fromUsers.selectors.selectAll);
     yield put(loadSuccess(users));
   } catch {
     yield put(loadFailure(GENERIC_ERROR));
