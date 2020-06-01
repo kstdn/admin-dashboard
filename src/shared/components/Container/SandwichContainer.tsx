56122ab7dc47f0ea1429fb21daecf488b947bf46
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-06-01 13:27:06 +0300

    FIx SandwichContainer

--- src/shared/components/Container/SandwichContainer.tsx
+++ src/shared/components/Container/SandwichContainer.tsx
@@ -2,9 +2,15 @@ import React, { ReactNode } from 'react';
 import styled from 'styled-components/macro';
 import { Stack } from '../Stack';
 
+const Header = styled.div`
+  flex-shrink: 0;
+`;
 const Content = styled.div`
   flex-grow: 1;
   overflow: auto;
+`;
+const Footer = styled.div`
+  flex-shrink: 0;
 `;
 
 type Props = {
@@ -18,16 +24,16 @@ type Props = {
  * Sandwich-style Container
  * When a header and/or footer is provided, they will be always visible
  * Its content
- * - is placed between the header and footer 
+ * - is placed between the header and footer
  * - will stretch to fill up the available space
  * - will show a scrollbar when there isn't any space left
  */
 const SandwichContainer = ({ header, content, footer, className }: Props) => {
   return (
     <Stack gap={true} className={className}>
-      {header}
+      <Header>{header}</Header>
       <Content>{content}</Content>
-      {footer}
+      <Footer>{footer}</Footer>
     </Stack>
   );
 };
