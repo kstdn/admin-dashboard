3c3d9e0e795e54ffcf04c0d8b089252a95251c43
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-06-02 20:52:06 +0300

    Return refresh handle from useLoadEntityPaginated

--- src/shared/hooks/useLoadEntityPaginated.ts
+++ src/shared/hooks/useLoadEntityPaginated.ts
@@ -1,6 +1,6 @@
 import { Paginated } from 'api/modules/shared/dto/Paginated';
 import { GENERIC_ERROR } from 'messages';
-import { Dispatch, SetStateAction, useEffect } from 'react';
+import { Dispatch, SetStateAction, useEffect, useCallback } from 'react';
 import { Status } from 'util/status';
 import { State } from './useEntityState';
 
@@ -10,38 +10,42 @@ export const useLoadEntityPaginated = <T>(
   page: number,
   limit: number,
   filter?: string,
-): void => {
+): { refresh: () => void } => {
 
-  useEffect(() => {
-    const load = async () => {
+  const load = useCallback(async () => {
+    setState({
+      items: [],
+      paginationData: undefined,
+      status: Status.Loading,
+      error: undefined,
+    });
+
+    try {
+      const { items, ...paginationData } = await loadFunc(page, limit, filter);
+
+      if (items) {
+        setState({
+          items,
+          paginationData,
+          status: Status.Resolved,
+          error: undefined,
+        });
+      }
+    } catch {
       setState({
         items: [],
         paginationData: undefined,
-        status: Status.Loading,
-        error: undefined,
+        status: Status.Rejected,
+        error: GENERIC_ERROR,
       });
-
-      try {
-        const { items, ...paginationData } = await loadFunc(page, limit, filter);
-
-        if (items) {
-          setState({
-            items,
-            paginationData,
-            status: Status.Resolved,
-            error: undefined,
-          });
-        }
-      } catch {
-        setState({
-          items: [],
-          paginationData: undefined,
-          status: Status.Rejected,
-          error: GENERIC_ERROR,
-        });
-      }
-    };
-
-    load();
+    }
   }, [page, limit, filter, setState, loadFunc]);
+
+  useEffect(() => {
+    load();
+  }, [load]);
+
+  return {
+    refresh: load
+  }
 };
