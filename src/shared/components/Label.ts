f7d9b190087bcbcdd0b568525b452fb00340308f
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-06-12 00:39:16 +0300

    Add Label component

---
+++ src/shared/components/Label.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/macro';
+
+export const Label = styled.div`
+  font-size: var(--font-size-minus-1);
+  text-transform: uppercase;
+  font-weight: var(--font-weight-accent);
+  letter-spacing: var(--letter-spacing);
+`;
