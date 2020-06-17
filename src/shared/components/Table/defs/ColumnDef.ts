2f0a92e79b2b02289f2d34809674e40f6c9dc936
Kostadin Kostov <6963245+kstdn@users.noreply.github.com>
2020-06-17 11:13:04 +0300

    Allow potentially empty cells in Table

--- src/shared/components/Table/defs/ColumnDef.ts
+++ src/shared/components/Table/defs/ColumnDef.ts
@@ -2,7 +2,7 @@ import { KeysForType } from 'util/types';
 import { CellComponentDef } from './CellComponentDef';
 
 export type ColumnDef<TData> = {
-  prop: KeysForType<TData, string>;
+  prop: KeysForType<TData, string | undefined>;
   name?: string;
   component?: CellComponentDef;
   align?: 'start' | 'center' | 'end';
